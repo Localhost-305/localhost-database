99245aed7ff7fe04109bee6c857512b2b62b52c1
Gabriel Depiro <gabriel.depiro9@gmail.com>
2024-09-12 22:11:54 -0300

    feature/LOC-19/atualizacao do script SQL

--- ddl.sql
+++ ddl.sql
@@ -2,6 +2,7 @@ CREATE DATABASE localhost305;
 
 CREATE TABLE users (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name_users VARCHAR(200) NOT NULL
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_on DATE NOT NULL,
@@ -11,7 +12,7 @@ CREATE TABLE users (
 CREATE TABLE dim_candidates (
     candidate_id INT AUTO_INCREMENT PRIMARY KEY,
     candidate_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     phone VARCHAR(13)  NOT NULL,
     birth_date DATE NOT NULL
 );
